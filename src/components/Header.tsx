47313a1e2d632dc1aa7c659ced9476d3d1e220a7
MessaOktafian <53614795+MessaOktafian@users.noreply.github.com>
2024-01-15 21:10:04 +0700

    bsbs
    bsbs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,16 +8,16 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
       <div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <NavLink className={styles.logo} to="/">
-            <img alt="Gamba logo" src="/logo-2.svg" />
+            <img alt="StakeSol logo" src="/logo.png" />
           </NavLink>
         </div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <div className={styles.externalLinks}>
-            <a href="https://t.me/strudelsonsolana" target="_blank" rel="noreferrer">
+            <a href="https://t.me/xxxxxxxx" target="_blank" rel="noreferrer">
               Telegram
             </a>
-            <a href="https://birdeye.so/token/2mnGSkXH1h6x5qmhwoQzAZDKa83vnRf8wNkNWVbdv7w5/7H5ijbPvBqRVBoDMtJhgQetxS9e2KFikP18JrabkctAS?chain=solana" target="_blank" rel="noreferrer">
-              Birdeye
+            <a href="https://dexscreen.com/xxxxxx" target="_blank" rel="noreferrer">
+              Dexscreen
             </a>
           </div>
           {children}
